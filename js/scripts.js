b1288132d1e5d276523260c8ecd34a34fed0cf34
Mitchellmbc <mitchelldye3@gmail.com>
2022-03-05 17:02:38 +0000

    Updated scripts.js to contain pokemonList containing 3 pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
+let pokemonList =[];
 
+pokemonList = [
+{name: "Bulbasaur", height: 0.7, types: ["Grass", "Poison"]},
+{name:"Charmander", height: 0.6, types: ["fire"]},
+{name: "Squirtle", height: 0.5, types: ["water"]}
+];
