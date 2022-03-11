1b019aef208fe222d3004bb3f9ab0b1e74ebdacb
Mitchellmbc <mitchelldye3@gmail.com>
2022-03-11 19:18:09 +0000

    Updated scripts.js IIFE works perfectly

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,28 @@
-let pokemonList =[];
+let pokemonRepository = (function(){
+  let pokemonList =[];
+  pokemonList = [
+  {name: "Bulbasaur", height: 0.7, types: ["Grass", "Poison"]},
+  {name:"Charmander", height: 0.6, types: ["fire"]},
+  {name: "Squirtle", height: 0.5, types: ["water"]}
+  ];
 
-pokemonList = [
-{name: "Bulbasaur", height: 0.7, types: ["Grass", "Poison"]},
-{name:"Charmander", height: 0.6, types: ["fire"]},
-{name: "Squirtle", height: 0.5, types: ["water"]}
-];
+  return {
+    add: function(pokemon){
+      pokemonList.push(pokemon);
+    },
+    getAll: function(){
+      return pokemonList;
+    }
+  };
+})();
 
+/*
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({name: "Diglet"});
+console.log(pokemonRepository);
+*/
+
+/*
 for(let i = 0; i < pokemonList.length; i++){
   document.write(`${pokemonList[i].name}   (height: ${pokemonList[i].height})`);
 
@@ -15,3 +32,4 @@ for(let i = 0; i < pokemonList.length; i++){
 
   document.write("<br>");
 }
+*/
